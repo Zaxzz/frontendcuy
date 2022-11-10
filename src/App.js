8e01c870031f97ea5b86b4f1b6ff77323e369f29
Zaxzz <117608875+Zaxzz@users.noreply.github.com>
2022-11-10 22:51:36 +0700

    bang lelah

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ import Program from './components/program'
 import Profil from './components/profil';
 import Galeri from './components/galeri';
 import Info from './components/info';
+import Landing from './components/landing';
 
 const App = () => {
   return (
     <div className=' object-cover text-cover'>
       <Navbar/>
       <Button/>
+      <Landing/>
       <div className="content bg-fixed bg-white" style={{backgroundImage: `url("https://i.top4top.io/p_2502kji0f1.png")`}}>
       <Profil/>
       
