17f1629970c84ab45fe7d8383b8c6dd4c5363ac1
PCdevv <103156588+PCdevv@users.noreply.github.com>
2022-11-10 10:06:08 +0700

    add about

--- src/components/about.js
+++ src/components/about.js
@@ -1,16 +1,46 @@
 const About = () => {
-    return (
-     
-        <div className="hero min-h-screen bg-white">
-  <div className="hero-content flex-col ">
-    <img src="https://placeimg.com/260/400/arch" className="max-w-sm shadow-2xl" />
-    <div>
-</div>
-</div>
-  </div>
-  
- 
-
+  return (
+      <div className="bg-white">
+          <div className="items-center text-center opacity-70 rounded-none rounded-tr-lg rounded-bl-lg card w-full shadow-2xl bg-green">
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text font-bold text-5x1 text-black">About</span>
+                </label>
+              </div>
+            </div>
+          </div>
+        <div class="hero min-h-screen">
+          <div class="hero-content flex-col lg:flex-row-reverse">
+            <div class="text-center lg:text-left">
+              <h1 class="text-5xl font-bold">Login now!</h1>
+              <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            </div>
+            <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+              <div class="card-body">
+                <div class="form-control">
+                  <label class="label">
+                    <span class="label-text">Email</span>
+                  </label>
+                  <input type="text" placeholder="email" class="input input-bordered" />
+                </div>
+                <div class="form-control">
+                  <label class="label">
+                    <span class="label-text">Password</span>
+                  </label>
+                  <input type="text" placeholder="password" class="input input-bordered" />
+                  <label class="label">
+                    <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
+                  </label>
+                </div>
+                <div class="form-control mt-6">
+                  <button class="btn btn-primary">Login</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
 }
 
