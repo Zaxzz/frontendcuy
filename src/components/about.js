40f965d640bd4c99b3f7473d0bfad1d78578d247
Jojowaro <117620257+Jojowaro@users.noreply.github.com>
2022-11-10 13:40:46 +0700

    galeri
    update galeri dan about

--- src/components/about.js
+++ src/components/about.js
@@ -1,13 +1,13 @@
-import about from "./assets/about.png";
-import css from "./assets/css.png";
-import figma from "./assets/figma.png";
-import flutter from "./assets/flutter.png";
-import html from "./assets/html.png";
-import js from "./assets/js.png";
-import php from "./assets/php.png";
-import unity from "./assets/unity.png";
-import unreal from "./assets/unreal.png";
-import vscode from "./assets/vscode.png";
+import about from "./asset/about.png";
+import css from "./asset/css.png";
+import figma from "./asset/figma.png";
+import flutter from "./asset/flutter.png";
+import html from "./asset/html.png";
+import js from "./asset/js.png";
+import php from "./asset/php.png";
+import unity from "./asset/unity.png";
+import unreal from "./asset/unreal.png";
+import vscode from "./asset/vscode.png";
 
 const About = () => {
   return (
@@ -15,14 +15,14 @@ const About = () => {
         <div className="items-center text-center opacity-70 rounded-none rounded-tr-lg rounded-bl-lg card w-full shadow-2xl bg-green">
           <div className="card-body">
               <label className="label">
-                <span className="label-text font-bold text-5x1 text-black">About</span>
+                <span className="label-text font-bold text-5xl text-black">About</span>
               </label>
           </div>
         </div>
         <div class="hero min-h-screen">
           <div class="hero-content flex-col lg:flex-row-reverse">
             <img src={about} alt="About"/>
-            <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-green2 border-2 rounded-3xl border-black">
+            <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-green border-2 rounded-3xl border-black text-black">
               <div class="card-body">
                 <p>
                 SMK Negeri 2 Magelang adalah sekolah yang terletak di Kota Magelang, Jawa Tengah.
@@ -36,13 +36,13 @@ const About = () => {
         <div className="items-center text-center opacity-70 rounded-none rounded-tr-lg rounded-bl-lg card w-full">
           <div className="card-body">
             <label className="label">
-              <span className="label-text font-bold text-5x1 text-black">What We Learn</span>
+              <span className="label-text font-bold text-5xl text-black">What We Learn</span>
             </label>
           </div>
         </div>
         <div class="hero min-h-screen">
           <div class="hero-content">
-            <div className="grid grid-cols-3 grid gap-4">
+            <div className="grid grid-cols-3 gap-4">
               <img src={html} alt="About"/>
               <img src={css} alt="About"/>
               <img src={js} alt="About"/>
