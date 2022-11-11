fa7f2bd8d8eb2103e0d37b2be76f91a7b09a3c7b
Zaxzz <117608875+Zaxzz@users.noreply.github.com>
2022-11-11 09:40:56 +0700

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,7 +10,11 @@ import { Link } from "react-scroll";
 const Footer = () => {
     return (
         <div id="footer">
-            <footer className="footer p-10 bg-green text-black">
+            <footer className="footer p-10 bg-white text-black">
+            <div className="items-center grid-flow-col">
+    <Link to="landing" smooth={true} duration={1000}><img className="btn bg-white border-none py-3 hover:bg-transparent" src={PPLG}/></Link>
+    <p className="font-bold">TIM PPLG <br/>Kelompok 2</p>
+  </div> 
   <div>
     <span className="footer-title text-black text-opacity-100">Services</span> 
     <a className="link link-hover">Branding</a> 
@@ -32,30 +36,6 @@ const Footer = () => {
     <a className="link link-hover">Cookie policy</a>
   </div>
 </footer> 
-<footer className="footer px-10 py-4 border-t bg-white text-black">
-  <div className="items-center grid-flow-col">
-    <Link to="landing" smooth={true} duration={1000}><img className="btn bg-white border-none py-3 hover:bg-transparent" src={PPLG}/></Link>
-    <p className="font-bold">TIM PPLG <br/>Kelompok 2</p>
-  </div> 
-  <div className="md:place-self-center md:justify-self-end">
-    <div className="grid grid-flow-col gap-4">
-      <ul className="menu menu-horizontal p-0">
-      <div class="btn hover:bg-transparent bg-transparent bg-opacity-40 border-none pt-5 mx-0 hover:scale-150">
-      <img className="w-full h-full" src={FB}/>
-      </div>
-      <div className="btn hover:bg-transparent bg-transparent bg-opacity-40 border-none pt-5 mx-0 hover:scale-150">
-      <img className="w-full h-full" src={TW}/>
-      </div>
-      <div className="btn hover:bg-transparent bg-transparent bg-opacity-40 border-none pt-5 mx-0 hover:scale-150">
-      <img className="w-full h-full" src={IG}/>
-      </div>
-      <div className="btn hover:bg-transparent bg-transparent bg-opacity-40 border-none pt-5 mx-0 hover:scale-150">
-      <img className="w-full h-full" src={YT}/>
-      </div>
-      </ul>
-    </div>
-  </div>
-</footer>
         </div>
     );
 } 
