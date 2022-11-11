5f6b92fb5a3a435a6fafbca899bde8febb3d5f9c
Zaxzz <117608875+Zaxzz@users.noreply.github.com>
2022-11-11 11:24:46 +0700

    sfsdf

--- src/components/landing.js
+++ src/components/landing.js
@@ -7,9 +7,9 @@ const Landing = () => {
       <div className="hero min-h-screen h-full bg-white" style={{ backgroundImage: `url("https://k.top4top.io/p_25056k6xu1.png")` }}>
 <div className="hero-overlay bg-opacity-60"></div>
 <div className="hero-content text-center text-neutral-content ">
-    <div className="absolute bottom-0 left-10">
+    <div className="absolute bottom-10 left-10">
   <div className=" bg-opacity-60 rounded-none rounded-tr-lg rounded-bl-lg card w-full shadow-2xl bg-green">
-  <div className="card-body">
+  <div className=" card-body">
     <div className="form-control">
       <div className="label flex  flex-col">
         <div className="label-text font-bold  text-white text-3xl">PENGEMBANGAN PERANGKAT</div>
